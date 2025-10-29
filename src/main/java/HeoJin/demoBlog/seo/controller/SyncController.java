d92b677348d507d69e3cd318308dc9f5333c105f
Jin <hurjin1109@naver.com>
2025-10-29 19:41:47 +0900

    FEAT : mongo 관련 의존성 추가 및 엔드포이트 지정 관련 오타 수정

--- src/main/java/HeoJin/demoBlog/seo/controller/SyncController.java
+++ src/main/java/HeoJin/demoBlog/seo/controller/SyncController.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class SyncController {
 
