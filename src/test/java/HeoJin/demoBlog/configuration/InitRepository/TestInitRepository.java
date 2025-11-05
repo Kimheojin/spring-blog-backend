a74ca50a26f438e53c6c296e9828cd6de9ee41a1
Jin <hurjin1109@naver.com>
2025-11-05 18:30:55 +0900

    FEAT : 쿼리 작성 방식 변경

--- src/test/java/HeoJin/demoBlog/configuration/InitRepository/TestInitRepository.java
+++ src/test/java/HeoJin/demoBlog/configuration/InitRepository/TestInitRepository.java
@@ -2,20 +2,31 @@ package HeoJin.demoBlog.configuration.InitRepository;
 
 import HeoJin.demoBlog.member.entity.Member;
 import HeoJin.demoBlog.member.entity.Role;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Repository
-public interface TestInitRepository {
+@Component
+@RequiredArgsConstructor
+public class TestInitRepository {
+
+    private final EntityManager em;
+
     // name 쿼리 사용
+    public Optional<Member> findByEmail(String email) {
+        return em.createQuery("select m from Member m join fetch m.role where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getResultStream()
+                .findFirst();
+    }
 
-    @Query("select m from Member m join fetch m.role where m.email = :email")
-    Optional<Member> findByEmail(@Param("email") String email);
-
-    @Query("select r from Role r where r.roleName = :roleName")
-    Optional<Role> findByRoleName(@Param("roleName") String roleName);
+    public Optional<Role> findByRoleName(String roleName) {
+        return em.createQuery("select r from Role r where r.roleName = :roleName", Role.class)
+                .setParameter("roleName", roleName)
+                .getResultStream()
+                .findFirst();
+    }
 
 }
