1e258093d15d0b22d22a1eedaf09d13ea1d36f56
Jin <hurjin1109@naver.com>
2025-11-06 22:09:26 +0900

    REFACTOR : aop 관련 중복 코드 수정

--- src/main/java/HeoJin/demoBlog/DemoBlogApplication.java
+++ src/main/java/HeoJin/demoBlog/DemoBlogApplication.java
@@ -2,11 +2,9 @@ package HeoJin.demoBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy // aop 로깅 관련 설정 추가
 @EnableScheduling
 public class DemoBlogApplication {
 	public static void main(String[] args) {
