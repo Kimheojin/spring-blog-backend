eb75f81284c976ef5e786f609343afe3f6885120
Jin <hurjin1109@naver.com>
2025-12-25 18:23:24 +0900

    CHORE : 부하 테스트 시 bean 제거 위해 profile 설정 추가

--- src/main/java/HeoJin/demoBlog/global/scheduler/seo/MongoScheduler.java
+++ src/main/java/HeoJin/demoBlog/global/scheduler/seo/MongoScheduler.java
@@ -3,11 +3,13 @@ package HeoJin.demoBlog.global.scheduler.seo;
 
 import HeoJin.demoBlog.seo.service.SyncService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile({"!test", "!performance"}) // 부하 테스트 시
 public class MongoScheduler {
 
     private final SyncService syncService;
